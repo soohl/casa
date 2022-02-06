2abd518fa722702e587c09b67989ac67f8b10993
soohl <soohl@kakao.com>
2022-02-05 20:51:38 -0500

    tdi

---
+++ python/stack-queue/23-implment_stack_using_queues.py
@@ -0,0 +1,31 @@
+"""
+Use queue to support stack operation
+
+Notes:
+push(x)
+pop() - remove first element
+top() - get first element
+empty()
+"""
+
+############ Stack Answer O(n) ########
+import collections
+
+class MyStack:
+    def __init__(self):
+        self.q = collections.deque()
+
+    def push(self, x):
+        self.q.append(x)
+        for _ in range(len(self.q) -1):
+            self.q.append(self.q.popleft())
+
+    def pop(self):
+        return self.q.popleft()
+
+    def top(self):
+        return self.q[0]
+
+    def empty(self):
+        return len(self.q) == 0
+########################################
