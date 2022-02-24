f1fdaa1f1c26694d478f39c12f0b8ac175168bb8
Soohyung Lee <soohl@kakao.com>
2022-02-24 00:06:28 -0500

    little

--- python/3-dict-sets/set.py
+++ python/3-dict-sets/set.py
@@ -1,1 +1,14 @@
 # Set theory
+
+# Removing duplicates
+l = ['a','a','b']
+list(set(l))
+
+# Set comprehension
+from unicodedata import name
+{chr(i) for i in range(32,256) if 'SIGN' in name(chr(i),'')}
+
+# Return value of dict view can be used as a set 
+d1 = dict(a=1,b=2,c=3,d=4)
+d2 = dict(b=20,d=40,e=50)
+d1.keys() & d2.keys() # {'b','d'}
